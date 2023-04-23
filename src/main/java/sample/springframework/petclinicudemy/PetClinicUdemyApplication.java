05b54fbf2de36d85952fbd0f242c1830d518a493
lyssatupy <lyssamt@gmail.com>
2023-04-23 11:26:30 -0500

    Initial commit

---
+++ src/main/java/sample/springframework/petclinicudemy/PetClinicUdemyApplication.java
@@ -0,0 +1,13 @@
+package sample.springframework.petclinicudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sample/springframework/petclinicudemy/PetClinicUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.springframework.petclinicudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
